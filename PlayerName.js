79c75b8b16663837c177cb90556bc088c927b3f4
prometea-afk <85350531+prometea-afk@users.noreply.github.com>
2021-06-04 17:23:36 +0200

    Add files via upload

---
+++ PlayerName.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function PlayerName(props) {
+  return (
+    <div>
+      <input value="Player #1" type="text" className="player-name" />
+    </div>
+  );
+}
+
+export default PlayerName;
